34d96b2c5f425723e2b29037cdef52841cf273e3
monly <vanan6b@gmail.com>
2025-03-06 10:16:36 +0700

    #3 - Facede Patterns

--- facade.with.patten.js
+++ facade.with.patten.js
@@ -6,3 +6,44 @@
  * Ứng dụng cuộc sống: Đằng sau bộ VEST
  * Đằng sau 1 hệ thống phức tạp là 1 giao diện đơn giản
 */
+
+class Discount{
+    calc(value) {
+        return value * 0.9
+    }
+}
+
+class Shipping{
+    calc(value) {
+        return 5
+    }
+}
+
+class Fees{
+    calc(value) {
+        return value * 1.05
+    }
+}
+
+class ShopeeFacadePattern {
+    constructor() {
+        this.discount = new Discount()
+        this.shipping = new Shipping()
+        this.fees = new Fees()
+    }
+
+    calc(price) {
+        price = this.discount.calc(price)
+        price = this.fees.calc(price)
+        price += this.shipping.calc()
+
+        return price
+    }
+}
+
+function buy(price) {
+    const shopee = new ShopeeFacadePattern()
+    console.log(`Peice:: `, shopee.calc(price))
+}
+
+buy(120000)
