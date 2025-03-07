f71111157f5bdd43987f2753e71126496d62bd48
monly <vanan6b@gmail.com>
2025-03-07 17:27:13 +0700

    update code

---
+++ proxy.with.patten.js
@@ -0,0 +1,8 @@
+/**
+ * Design Patterns - #4 - Proxy Patterns
+ * Độ khó khi học: 3
+ * Sử dụng dự án: 4
+ * Ứng dụng phần mềm: Tăng lương lập trình viên
+ * Ứng dụng cuộc sống: Dịch vụ môi giới
+ * 
+*/
